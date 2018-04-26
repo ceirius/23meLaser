00ce80d36395e002e2d5e07b6eb924134f8ce4aa
ceirius <shreyas.krishnan@gmail.com>
2018-04-25 23:40:45 -0500

    update lasermaze23meV2
    V3 on local drive

--- lasermaze23meV2.py
+++ lasermaze23meV2.py
@@ -4,63 +4,7 @@
 
 
 
-#import argparse
-#
-#def run(args):
-#	filename = open(args.input) 										
-#	output_filename = args.output 										
-#	fout = open(args.output, 'w')
-#	fout.write(step_count, '\n')
-#	fout.write(start_x, start_y)
-#	fout.close()
-	
-
-#def main():
-#	parser=argparse.ArgumentParser(description="Fire a laser through a maze")
-#	parser.add_argument("-in",help="txt input file" ,dest="input", type=str, required=True)
-#	parser.add_argument("-out",help="txt output filename" ,dest="output", type=str, required=True)
-#	parser.set_defaults(func=run)
-#	args=parser.parse_args()
-#	args.func(args)
-#
-#if __name__=="__main__":
-#	main() 
-
-
-filename = input("enter a valid file name:   ")
-fileinp = open(filename,'r')
-input_data = []																			# to hold all data for easy parsing
-
-
-for line in fileinp:
-	data = line.split(" ")							
-	input_data.append(data)
-
-x = int(input_data[0][0])
-y = int(input_data[0][1])
-start_x = int(input_data[1][0])															# coordinates for start position and subsequent player movements
-start_y = int(input_data[1][1])
-direction = input_data[1][2].rstrip()													# direction of laser
-
-
-maze = [0] * int(x)																		# creating 2D list/array
-for i in range(int(x)):																	# The matrix is created here and populated by 0s 
-	maze[i] = [0] * int(y)
-
-	
-count = len(input_data)
-
-for i in range(2, count - 1):															# assigning mirrors to matrix
-	mirror_x = int(input_data[i][0])													# mirror coordinates
-	mirror_y = int(input_data[i][1])
-	#print (mirror_x, mirror_y)
-	maze[mirror_x][mirror_y] = input_data[i][2].rstrip()
-	
-
-print(maze, "\n", maze[mirror_x][mirror_y], input_data[3][2], "count = ",count)
-# define function or object that instructs conditions for reaction between laser and mirror and resulting direction	
-
-step_count = 0
+import argparse
 
 def west(direction, start_x, start_y, step_count):
 	if start_x - 1 >= 0:																# testing if within bounds
@@ -169,22 +113,83 @@ def south(direction, start_x, start_y, step_count):
 def end(start_x, start_y, step_count):
 	if (start_x == 0 or start_x == x-1 or start_y == 0 or start_y == y-1):				# if player exits any matrix boundary
 		print (step_count, "\n", start_x, " , ", start_y)
-		#run(step_count, start_x, start_y)
+		run(step_count, start_x, start_y)
 		print("five")
 	else:
 		print ("-1", "\n", start_x, " , ", start_y)
-		#run(step_count, start_x, start_y)
+		run(step_count, start_x, start_y)
 		print("six")
 		
-			
-if start_x >= 0 or start_x < x or start_y >= 0 or start_y < y and step_count < 20:		# while player is in matrix bounds and step count < 20 
-	if direction == 'S':																# call respective direction functions 
-		south(direction, start_x, start_y, step_count)
-	elif direction == 'N':
-		north(direction, start_x, start_y, step_count)
-	elif direction == 'E':
-		east(direction, start_x, start_y, step_count)
-	elif direction == 'W':
-		west(direction, start_x, start_y, step_count)	
+def run(args):
+	filename = open(args.input) 										
+	output_filename = args.output 										
+	fout = open(args.output, 'w')
+	#fout.write(str(step_count), '\n')
+	#fout.write(str(start_x), str(start_y))
+	fout.close()
+	
+
+def main():
+	parser=argparse.ArgumentParser(description="Fire a laser through a maze")
+	parser.add_argument("-in",help="txt input file" ,dest="input", type=str, required=True)
+	parser.add_argument("-out",help="txt output filename" ,dest="output", type=str, required=True)
+	#parser.set_defaults(func=run)
+	args=parser.parse_args()
+	#args.func(args)
+
+
+	#filename = input("enter a valid file name:   ")
+	#print(args.input)
+	fileinp = open(args.input,'r')
+	input_data = []																			# to hold all data for easy parsing
+
+
+	for line in fileinp:
+		data = line.split(" ")							
+		input_data.append(data)
+
+	x = int(input_data[0][0])
+	y = int(input_data[0][1])
+	start_x = int(input_data[1][0])															# coordinates for start position and subsequent player movements
+	start_y = int(input_data[1][1])
+	direction = input_data[1][2].rstrip()													# direction of laser
+
+
+	maze = [0] * int(x)																		# creating 2D list/array
+	for i in range(int(x)):																	# The matrix is created here and populated by 0s 
+		maze[i] = [0] * int(y)
 
 		
+	count = len(input_data)
+
+	for i in range(2, count - 1):															# assigning mirrors to matrix
+		mirror_x = int(input_data[i][0])													# mirror coordinates
+		mirror_y = int(input_data[i][1])
+		#print (mirror_x, mirror_y)
+		maze[mirror_x][mirror_y] = input_data[i][2].rstrip()
+		
+
+	for i in range(int(x)):
+		print(maze[i])
+		
+	#print(maze)
+	print(maze[mirror_x][mirror_y] + "\t" + input_data[3][2], "count = ",count)
+	# define function or object that instructs conditions for reaction between laser and mirror and resulting direction	
+
+	step_count = 0
+
+				
+	if start_x >= 0 or start_x < x or start_y >= 0 or start_y < y and step_count < 20:		# while player is in matrix bounds and step count < 20 
+		if direction == 'S':																# call respective direction functions 
+			south(direction, start_x, start_y, step_count)
+		elif direction == 'N':
+			north(direction, start_x, start_y, step_count)
+		elif direction == 'E':
+			east(direction, start_x, start_y, step_count)
+		elif direction == 'W':
+			west(direction, start_x, start_y, step_count)	
+	
+if __name__=="__main__":
+	main() 
+	
+
